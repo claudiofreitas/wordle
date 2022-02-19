f704a981509e702032ab844c5024bd63730e62a6
Claudio Freitas <claudiofreitas3@gmail.com>
2022-02-20 02:48:00 +0900

    Add prettier

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-  </React.StrictMode>,
+  <React.StrictMode></React.StrictMode>,
   document.getElementById('root')
 );
