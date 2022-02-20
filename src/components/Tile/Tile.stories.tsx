93f3ce68e89a6ea711ffcc8c27431a97e75a093b
Claudio Freitas <claudiofreitas3@gmail.com>
2022-02-20 20:57:29 +0900

    Complete Tile variations

--- src/components/Tile/Tile.stories.tsx
+++ src/components/Tile/Tile.stories.tsx
@@ -4,11 +4,32 @@ export default {};
 
 export const Default = () => {
   return (
-    <>
-      <Tile>t</Tile>
+    <div>
+      <Tile state="correct">t</Tile>
       <Tile>e</Tile>
-      <Tile>s</Tile>
-      <Tile>t</Tile>
-    </>
+      <Tile state="present">s</Tile>
+      <Tile state="absent">t</Tile>
+      <Tile />
+    </div>
   );
+};
+
+export const Correct = () => {
+  return <Tile state="correct">w</Tile>;
+};
+
+export const Empty = () => {
+  return <Tile>w</Tile>;
+};
+
+export const Present = () => {
+  return <Tile state="present">w</Tile>;
+};
+
+export const Absent = () => {
+  return <Tile state="absent">w</Tile>;
+};
+
+export const Undisclosed = () => {
+  return <Tile state="undisclosed">w</Tile>;
 };
--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,25 +1,61 @@
-import { FC } from 'react';
+import { CSSProperties, FC } from 'react';
 
-const Tile: FC = ({ children }) => {
+type TileState = 'correct' | 'present' | 'absent' | 'empty' | 'undisclosed';
+
+interface Props {
+  state?: TileState;
+}
+
+const Tile: FC<Props> = ({ children, state = 'empty' }) => {
+  const baseStyle: CSSProperties = {
+    // Layouts
+    display: 'inline-flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    boxSizing: 'border-box',
+    width: 62,
+    height: 62,
+
+    // Text
+    fontFamily: '"Clear Sans", "Helvetica Neue", "Arial", "sans-serif"',
+    textTransform: 'uppercase',
+    fontSize: 32,
+    fontWeight: 700,
+
+    // Appearance
+    borderWidth: 2,
+    borderStyle: 'solid',
+    borderColor: 'transparent',
+  };
+
+  const styleByState: Record<TileState, CSSProperties> = {
+    correct: {
+      color: '#ffffff',
+      backgroundColor: '#6aaa64',
+    },
+    present: {
+      color: '#ffffff',
+      backgroundColor: '#c9b458',
+    },
+    absent: {
+      color: '#ffffff',
+      backgroundColor: '#787c7e',
+    },
+    empty: {
+      color: '#000000',
+      borderColor: '#d3d6da',
+    },
+    undisclosed: {
+      color: '#000000',
+      borderColor: '#000000',
+    },
+  };
+
   return (
     <div
       style={{
-        // Layouts
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: 62,
-        height: 62,
-
-        // Text
-        fontFamily: '"Clear Sans", "Helvetica Neue", "Arial", "sans-serif"',
-        textTransform: 'uppercase',
-        fontSize: 32,
-        fontWeight: 700,
-
-        // Appearance
-        color: '#ffffff',
-        backgroundColor: '#6aaa64',
+        ...baseStyle,
+        ...styleByState[state],
       }}
     >
       {children}
