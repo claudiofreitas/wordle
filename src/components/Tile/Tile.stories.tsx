82c2b66be4a362cd1155e496364e6a96338d94b9
Claudio Freitas <claudiofreitas3@gmail.com>
2022-02-20 20:11:50 +0900

    Success tile basic style

--- src/components/Tile/Tile.stories.tsx
+++ src/components/Tile/Tile.stories.tsx
@@ -3,5 +3,12 @@ import Tile from './Tile';
 export default {};
 
 export const Default = () => {
-  return <Tile>a</Tile>;
+  return (
+    <>
+      <Tile>t</Tile>
+      <Tile>e</Tile>
+      <Tile>s</Tile>
+      <Tile>t</Tile>
+    </>
+  );
 };
--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,7 +1,30 @@
 import { FC } from 'react';
 
-const Tile: FC = () => {
-  return <div>a</div>;
+const Tile: FC = ({ children }) => {
+  return (
+    <div
+      style={{
+        // Layouts
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 62,
+        height: 62,
+
+        // Text
+        fontFamily: '"Clear Sans", "Helvetica Neue", "Arial", "sans-serif"',
+        textTransform: 'uppercase',
+        fontSize: 32,
+        fontWeight: 700,
+
+        // Appearance
+        color: '#ffffff',
+        backgroundColor: '#6aaa64',
+      }}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Tile;
